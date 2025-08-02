556661042e9021fb7a4802da736ca688a5f3284c
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2025-08-02 19:20:08 +0530

    Update ExpiringIn3Days.jsx

--- frontend/src/pages/ExpiringIn3Days.jsx
+++ frontend/src/pages/ExpiringIn3Days.jsx
@@ -5,7 +5,8 @@ import { useMyContext } from '../Context/AppContext';
 import { useNavigate } from 'react-router-dom';
 
 const ExpiringMembers = () => {
-  const { member } = useMyContext();
+  const { member,Allmember } = useMyContext();
+  Allmember();
   const navgaite = useNavigate();
 
   const today = new Date();
@@ -79,3 +80,4 @@ const ExpiringMembers = () => {
 };
 
 export default ExpiringMembers;
+
